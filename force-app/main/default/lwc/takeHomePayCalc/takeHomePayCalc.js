c38549c0445b7671a4653dfbd3e31da8d06a8479
Adrienne D. Millican <39879592+admillican08@users.noreply.github.com>
2024-09-16 21:49:42 -0600

    Interim commit

--- force-app/main/default/lwc/takeHomePayCalc/takeHomePayCalc.js
+++ force-app/main/default/lwc/takeHomePayCalc/takeHomePayCalc.js
@@ -1,86 +1,129 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getTaxRatePercentage  from '@salesforce/apex/TaxMetadataController.getTaxRatePercentage';
+import getFederalRate  from '@salesforce/apex/FederalRatesMetadataController.getFederalRate';
 
-export default class PlatformDevCertCalc extends LightningElement {
-    devFundamentalScore = 50;
-    procAutomationScore = 50;
-    userInterfaceScore = 50;
-    testDebugDeployScore = 50;
-    numberOfQuestions = 60;
+export default class takeHomePayCalc extends LightningElement {
+    @api recordId;
+    @api fieldName = 'Salary__cc';
+    salaryTypeVal = '';
+    payFrequencyVal = '';
+    filingStatusVal = '';
+    salAmt = 0.0;
+    taxBr = 0.0;
+    medicareRate = 0.0;
+    socSecRate = 0.0;
+    error = '';
+    addtlDeductAmt = 0.0;
+    addtlWithholdAmt = 0.0;
+    currYr = 0;
+    
+    takeHomePay = 0.00;
 
-    certificationScore = 50;
+    fedExemptValue = false;
+    medExemptValue = false;
+    socExemptValue = false;
 
-    showResources = false;
-    showGoodJob = false;
+    federalRates = [ { name: 'Medicare', rate: 0.0},
+                    { name: 'Social Security', rate: 0.0}];
  
-    generateRandom(){
-        return Math.floor(Math.random() * 100);
+    get salaryOptions(){
+        return [
+            { label: 'Hourly', value: 'Hourly' },
+            { label: 'Salaried', value: 'Salaried' },
+            { label: 'Lump Sum', value: 'Lump' }
+        ];
     }
- 
-    @track attemptHistory = [];
-
-    startingVals = [1,2,3,4];
-    connectedCallback(){
-        let dummyAttempt = {};
-        for(let i=0; i<this.startingVals.length; i++){
-            dummyAttempt = {Id: this.startingVals[i], Score: this.generateRandom()}
-            this.attemptHistory.push(dummyAttempt);
-        }
-    }
-
-    calculateScore(){
-        this.certificationScore = (this.devFundamentalScore * 0.23) + 
-        (this.procAutomationScore * 0.30) + (this.userInterfaceScore * 0.25) 
-        + (this.testDebugDeployScore * 0.22);
-        if(this.certificationScore < 0 || this.certificationScore > 100){
-            alert("Certification score error. Please check your inputs");
-            this.certificationScore = 0;
-        }
-       this.showResourcesIfFailed();
-       this.addAttemptHistory(this.certificationScore);
+    
+    get payFreqOptions(){
+        return [
+            { label: 'Paid Once', value: 'Once' },
+            { label: 'Weekly', value: 'Weekly' },
+            { label: 'Biweekly', value: 'Biweekly' },
+            { label: 'Semimonthly', value: 'Semi' },
+            { label: 'Monthly', value: 'Monthly' }
+        ];
     }
 
-    handleChange(event){
+      get filingStatusOptions(){
+        return [
+            { label: 'Single', value: 'S' },
+            { label: 'Married Filing Jointly', value: 'MFJ' },
+            { label: 'Married Filling Separately', value: 'MFS' },
+            { label: 'Head of Household', value: 'HOH' },
+        ];
+    }
+
+       get radioOptions(){
+        return [
+            { label: 'Yes', value: 'Y' },
+            { label: 'No', value: 'N'},
+        ];
+    }
+
+     getCurrentYear(){
+        this.currYr = new Date().getFullYear();
+    }
+
+    @wire(getTaxRatePercentage, { filingStatusVal: '$filingStatusVal', salAmt: '$salAmt', currYr: '$currYr' }) taxBr 
+    ({ error, data }) {
+       if (data) {
+           this.taxBr = data;
+       } else if (error) {
+           this.error = error;
+           alert(error);
+    }
+   }
+
+     @wire(getFederalRate, { rateType: '$rateType', currYr: '$currYr' }) federalRates
+    ({ error, data }) {
+       if (data) {
+           this.rate = data;
+       } else if (error) {
+           this.error = error;
+           alert(error);
+    }
+   }
+    
+      handleSalaryTypeChange(event){
+        this.salaryTypeVal = event.detail.value;
+    }
+
+     handlePayFreqChange(event){
+        this.payFrequencyVal = event.detail.value;
+    }
+
+     handleFilingStatusChange(event){
+        this.filingStatusVal = event.detail.value;
+    }
+
+    handleNumericChange(event){
         const inputName = event.target.name;
         let value = Number(event.target.value);
-        if(inputName === 'devFundamentals'){
-                this.devFundamentalScore = value;
-        } else if (inputName === 'procAutomation'){
-                this.procAutomationScore = value;
-        } else if (inputName === 'userInterface'){
-                this.userInterfaceScore = value;
-        } else if (inputName === 'testDebugDeploy'){
-                this.testDebugDeployScore = value;
+        if (inputName === 'salaryAmount'){
+            this.salAmt = value;
+        } else if (inputName === 'addtlDeductAmt'){
+            this.addtlDeductAmt = value;
+        } else if (inputName === 'addtlWithholdAmt'){
+            this.addtlWithholdAmt = value;
         } else {
             alert('Error with input number');
         }
     }
 
+    handleExemptFederalChange(event){
+        this.fedExemptValue = event.target.value;
+    }
+
+    handleExemptMedicareChange(event){
+        this.medExemptValue = event.target.value;
+    }
+
+    handleExemptSocChange(event){
+        this.socExemptValue = event.target.value;
+    }
+
     handleClick(event){
-        this.calculateScore();
+        alert('Tax bracket: ' + this.taxBr);
     }
-
-    showResourcesIfFailed(){
-        if(this.certificationScore < 68){
-            this.showResources = true;
-        } else {
-            this.showResources = false;
-        }
-        this.showGoodJob = !this.showResources;
-    }
-
-        addAttemptHistory(score){
-        let attemptNum = (this.attemptHistory.length + 1);
-        const attempt = {
-                            Id:attemptNum, Score:score
-                        }
-        this.attemptHistory = [...this.attemptHistory, attempt];
-    }
-
-    deleteAttemptHandler(event){
-        let attemptNum = event.detail;
-        this.attemptHistory = this.attemptHistory.filter(attempt => attempt.Id != attemptNum );
-        console.log('New attempt history: ' + this.attmeptHistory);
-    }
-
   
 }
